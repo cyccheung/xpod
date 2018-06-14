3f7b78730554dea0aafc67a23aa842adeb5dd386
cuteseaurchin <cuteseaurchin@github.com>
2018-06-14 10:29:35 +0800

    Standardized input parameter and added get functions

--- Arena.cpp
+++ Arena.cpp
@@ -1,18 +1,25 @@
 #include "Arena.h"
 #include <iostream>
 
-//Default constructor creates 9x6 arena
+//Default constructor creates 9x6 arena with 3 obstacles at 2,3 3,5 1,6
 Arena::Arena()
-    : length(9), width(6) {}
+    : length(9), width(6) {
+        std::pair<int,int> obstacle1 = std::make_pair(2,3);
+        std::pair<int,int> obstacle2 = std::make_pair(3,5);
+        std::pair<int,int> obstacle3 = std::make_pair(1,6);
+        obstacles.push_back(obstacle1);
+        obstacles.push_back(obstacle2);
+        obstacles.push_back(obstacle3);
+    }
 
 //Custom constructor creates specified arena
-Arena::Arena(const int lengthIn, const int widthIn)
+Arena::Arena(const int &lengthIn, const int &widthIn)
     : length(lengthIn), width(widthIn) {}
 
 //Returns whether the specified position is obstacle free
-bool Arena::openSquare(const int &row, const int &column) {
-    for(int i = 0; i < obstacles.size(); ++i) {
-        if(obstacles.at(i).first == row && obstacles.at(i).second == column) {
+bool Arena::openSquare(const std::pair<int,int> &position) const {
+    for(int i = 0; i < (int)obstacles.size(); ++i) {
+        if(obstacles.at(i).first == position.first && obstacles.at(i).second == position.second) {
             return false;
         }
     }
@@ -25,7 +32,49 @@ bool Arena::openSquare(const int &row, const int &column) {
 3 1     //Row 3, column 1
 */
 const void Arena::printObstacles() {
-    for(int i = 0; i < obstacles.size(); ++i) {
-        std::cout << obstacles.at(i).first << " " << obstacles.at(i).second << "\n"
+    for(int i = 0; i < (int)obstacles.size(); ++i) {
+        std::cout << obstacles.at(i).first << " " << obstacles.at(i).second << "\n";
     }
+}
+
+std::vector<std::pair<int, int> > Arena::getObstacles() const {
+    return obstacles;
+}
+
+const bool Arena::ifHome(const std::pair<int,int> &position, const bool player) {
+    //If player 1's unit
+    if(player) {
+        //If unit is in row 8 or more
+        if(position.first >= (length - 1)) {
+            return true;
+        }
+    }
+    //If player 2's unit
+    else {
+        //If unit is in row 0 or less
+        if(position.first <= 0) {
+            return true;
+        }
+    }
+    //Otherwise return false
+    return false;
+}
+
+const bool Arena::edgeOfMap(const std::pair<int,int> &position) const {
+    if(position.first <= -1 || position.first >= length) {
+        return true;
+    }
+    else if(position.second <= -1 || position.second >= width) {
+        return true;
+    }
+    return false;
+}
+
+
+int Arena::getRows() const {
+    return length;
+}
+
+int Arena::getColumns() const {
+    return width;
 }
--- Arena.h
+++ Arena.h
@@ -7,6 +7,8 @@ Default constructor creates 9x6 arena. Custom constructor takes in rows x column
 Maximum size set as 27 long x 12 wide.
 Stores obstacles as a vector of pairs. Each pair contains the <row, column> of each
 obstacle.
+Bottom left position is (0,0).
+Player 1 home row is row 0 and Player 2's is row length (member variable)
 */
 
 #include <vector>
@@ -20,7 +22,7 @@ public:
     Arena(const int &lengthIn, const int &widthIn);
 
     //Returns whether the specified position is obstacle free
-    bool openSquare(const int &row, const int &column);
+    bool openSquare(const std::pair<int,int> &position) const;
 
     //Print out obstacles in the following format:
     /*
@@ -28,6 +30,24 @@ public:
     3 1     //Row 3, column 1
     */
     const void printObstacles();
+
+    //Returns vector of obstacles
+    std::vector<std::pair<int, int> > getObstacles() const;
+
+    //Returns whether or not position is an opponent home row. player input parameter
+    //is the player whose turn it is, is true if player 1, false if player 2
+    const bool ifHome(const std::pair<int,int> &position, const bool player);
+
+    //Returns whether or not position is edge of map. Returns true for positions outside the arena
+    //Returns false for positions on the border (i.e 0,0 and 3,2 will return false for a 4x3 arena)
+    const bool edgeOfMap(const std::pair<int,int> &position) const;
+
+    //Returns number of rows
+    int getRows() const;
+
+    //Returns numbers of columns
+    int getColumns() const;
+
 private:
     int length; //Rows
     int width;  //Columns
